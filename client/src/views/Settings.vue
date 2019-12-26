5c1bad1888b5903e8e4e3aeb38b15125d56068f7
Matthew Ma <matthew.ma@kitware.com>
2019-12-26 16:12:33 -0500

    Initial option to toggle theme

--- client/src/views/Settings.vue
+++ client/src/views/Settings.vue
@@ -5,12 +5,25 @@ import NavigationBar from "@/components/NavigationBar";
 import AttributeSettings from "@/components/AttributeSettings";
 
 export default {
-  name: "Config",
+  name: "Settings",
   components: { NavigationBar, AttributeSettings },
   inject: ["girderRest"],
-  data: () => ({}),
-  computed: {},
-  methods: {}
+  data() {
+    let lightTheme = true;
+    try {
+      lightTheme = JSON.parse(localStorage.getItem("viameweb-light-theme"));
+    } catch (ex) {} // eslint-disable-line
+    this.$vuetify.theme.dark = !lightTheme;
+    return { lightTheme };
+  },
+  watch: {
+    lightTheme(value) {
+      this.$vuetify.theme.dark = !value;
+      try {
+        localStorage.setItem("viameweb-light-theme", value);
+      } catch (ex) {} // eslint-disable-line
+    }
+  }
 };
 </script>
 
@@ -18,7 +31,15 @@ export default {
   <v-content>
     <NavigationBar />
     <v-container>
-      <AttributeSettings />
+      <v-card class="color-setting">
+        <v-card-title class="pb-0">
+          Colors
+        </v-card-title>
+        <v-card-text>
+          <v-switch v-model="lightTheme" label="Light theme"></v-switch>
+        </v-card-text>
+      </v-card>
+      <AttributeSettings class="mt-4" />
     </v-container>
   </v-content>
 </template>
